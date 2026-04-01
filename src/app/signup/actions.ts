c35e0f73ac9168f0a3f30e61eb3f64ec905446c2
aui19f <aui19f@gmail.com>
2026-04-01 21:29:16 +0900

    feat: GA4 회원가입 이벤트 추적 로직 추가 (useEffect)

--- src/app/signup/actions.ts
+++ src/app/signup/actions.ts
@@ -19,26 +19,5 @@ export async function tempSignUp(prev: unknown, formData: FormData) {
     return { success: false, message: authError.message };
   }
   console.log("authData", authData);
-  // 2. Auth 생성 성공 시, 해당 유저의 ID를 가져와 Prisma로 프로필 생성
-  if (authData.user) {
-    try {
-      await prisma.profiles.create({
-        data: {
-          id: authData.user.id, // Auth 유저 ID와 동일하게 설정 (FK 역할)
-          email: email,
-          nickname: "nicknameTest",
-          role: "user",
-        },
-      });
-      return { success: true, message: "회원가입 및 프로필 생성 완료!" };
-    } catch (dbError) {
-      console.error("DB 저장 에러:", dbError);
-      return {
-        success: false,
-        message: "인증은 성공했으나 프로필 생성에 실패했습니다.",
-      };
-    }
-  }
-
-  return { success: false, message: "알 수 없는 에러 발생" };
+  return { success: true, message: "회원가입  완료!" };
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 import { tempSignUp } from "@/app/signup/actions";
-import { useActionState } from "react";
+import { sendGAEvent } from "@next/third-parties/google";
+import { useActionState, useEffect } from "react";
 
 export default function SignUp() {
   const [state, formAction] = useActionState(tempSignUp, null);
+  useEffect(() => {
+    // state가 존재하고, 회원가입이 성공(success: true)했을 때만 전송
+    if (state?.success) {
+      sendGAEvent("event", "sign_up", {
+        method: "email",
+      });
+      console.log("GA4: 회원가입 이벤트 전송됨");
+    }
+  }, [state]); // state가 바뀔 때마다 실행
+
   return (
     <form action={formAction}>
       <input name="email" type="text" placeholder="email" />
